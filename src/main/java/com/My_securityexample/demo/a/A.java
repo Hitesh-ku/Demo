a0355664f62fa172c2ec8cc411386752e22dc2a2
Hitesh Kumar <hk3696294@gmail.com>
2026-03-28 12:32:34 +0530

    Update A.java

--- src/main/java/com/My_securityexample/demo/a/A.java
+++ src/main/java/com/My_securityexample/demo/a/A.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 public class A {
     public static void main(String[] args) {
+        // this is changed.
 
         List<Integer> val = Arrays.asList(13,25,6,8,25,43,26);
         List<Integer> r = val.stream().map(x->x+10).collect(Collectors.toList());
